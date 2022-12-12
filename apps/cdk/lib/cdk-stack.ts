68dc2a8ff6de23fc8282fb1c22ab78b7ec81eeb4
Ben Osborn <bengrantalbertosborn@gmail.com>
2022-12-13 08:44:35 +1100

    Created AWS resources

--- apps/cdk/lib/cdk-stack.ts
+++ apps/cdk/lib/cdk-stack.ts
@@ -1,16 +1,19 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
 
 export class CdkStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        // The code that defines your stack goes here
+        // Core storage infrastructure
+        const bucket = new s3.Bucket(this, "imageBucket", {});
+        const table = new dynamodb.Table(this, "cardsTable", { partitionKey: { name: "id", type: dynamodb.AttributeType.STRING } });
 
-        // example resource
-        // const queue = new sqs.Queue(this, 'CdkQueue', {
-        //   visibilityTimeout: cdk.Duration.seconds(300)
-        // });
+        // Create main API
+        const api = new apigateway.RestApi(this, "restApi", {});
+        const cardsResource = api.root.addResource("cards");
     }
 }
